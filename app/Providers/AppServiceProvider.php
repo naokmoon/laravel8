e42c45e8c243537877343a65501282ba74e4ebc0
jddumas <jddumas@cooptel.coop>
2022-12-02 14:04:35 -0500

    Service Containers, Dependency Injections

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Models\BlogPost;
 use App\Models\Comment;
 use App\Observers\BlogPostObserver;
 use App\Observers\CommentObserver;
+use App\Services\Counter;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -39,13 +40,31 @@ class AppServiceProvider extends ServiceProvider
         Blade::aliasComponent('components.comment_form', 'commentForm');
         Blade::aliasComponent('components.comment_list', 'commentList');
 
-        // ViewComposers
+        // View Composers
         // view()->composer('*', ActivityComposer::class); // Can pass to all view
         view()->composer(['posts.index', 'posts.show'], ActivityComposer::class);
 
         // Observers
         BlogPost::observe(BlogPostObserver::class);
         Comment::observe(CommentObserver::class);
+
+        // Service Containers / Dependency Injections (for Controller Constructors by example)
+        $this->app->singleton(Counter::class, function ($app) {
+            return new Counter(
+                $app->make('Illuminate\Contracts\Cache\Factory'),
+                $app->make('Illuminate\Contracts\Session\Session'),
+                env('COUNTER_TIMEOUT')
+            );
+        });
+            // or ->bind for new Instance() every time it's called
+
+        $this->app->singleton(Counter::class, function ($app) {
+            return new Counter(env('COUNTER_TIMEOUT'));
+        });
+
+        // $this->app->when(Counter::class)
+        //     ->needs('$timeout')
+        //     ->give(env('COUNTER_TIMEOUT'));
 
         // Schema::defaultStringLength(191);
     }
